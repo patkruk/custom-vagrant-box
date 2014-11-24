6bbb9af3502d05ac6e617a145df34437d764a4cb
Patryk Kruk <patryk@getitllc.com>
2014-11-24 13:52:52 -0500

    Updated the fabric shell script

--- puphpet/files/exec-always/fabric.sh
+++ puphpet/files/exec-always/fabric.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-pip install fabric
+# Fabric's dependencies
+sudo pip install boto
+sudo pip install paramiko==1.10
+sudo pip install pyyaml
+sudo pip install Mako
+sudo pip install fogbugz
+
+# Fabric
+sudo pip install fabric
