0d0dcccee3642df61f20f487784f63e4849f978d
Patryk Kruk <patryk@getitllc.com>
2014-12-04 16:03:13 -0500

    Updated some of the versions

--- puphpet/files/exec-always/fabric.sh
+++ puphpet/files/exec-always/fabric.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 # Fabric
-sudo pip install fabric
+sudo pip install fabric==1.5.1
 echo ******************** fabric installed ********************
 
 # Fabric's post-install dependencies
-sudo pip install pycrypto==2.1
-echo ******************** pycrypto installed ********************
 sudo pip install boto
 echo ******************** boto installed ********************
 sudo pip install paramiko==1.10
@@ -17,3 +15,5 @@ sudo pip install Mako
 echo ******************** mako installed ********************
 sudo pip install fogbugz
 echo ******************** fogbugz installed ********************
+sudo pip install pycrypto==2.1
+echo ******************** pycrypto installed ********************
